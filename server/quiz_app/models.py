01a4941f0e8f4d50333714dcae48bd10a77afcd4
r_dossayev <dosaev.rysbek5@gmsil.com>
2023-05-10 11:31:48 +0600

    start

--- server/quiz_app/models.py
+++ server/quiz_app/models.py
@@ -1,16 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
-# class Quiz(models.Model):
-#     name = models.CharField(max_length=50)
+# class MyUser(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
 #     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Quiz(models.Model):
+    name = models.CharField(max_length=250)
+    poster = models.ImageField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Question(models.Model):
     questionText = models.CharField(max_length=250)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Answer(models.Model):
-    answer = models.CharField(max_length=250)
-    question = models.ForeignKey(Quiz, on_delete=models.RESTRICT)
+    textAnswer = models.CharField(max_length=250)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    isCorrect = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
